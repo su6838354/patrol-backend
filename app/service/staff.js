e57c4e33d2d60d80966e1e8018bb377b63ff5260
meichangsu <meichangsu@pinduoduo.com>
2018-07-07 01:02:14 +0800

    update

--- app/service/staff.js
+++ app/service/staff.js
@@ -8,6 +8,25 @@ const BaseService = ServiceFactory('staff');
 
 class StaffService extends BaseService {
 
+    async list(params) {
+        const { limit=10, offset = 0 } = params;
+        delete params.limit;
+        delete params.offset;
+        const res = await this.app.mysql.select('staff', {
+            where: { ...params },
+            orders: [['id','desc']],
+            limit,
+            offset,
+        });
+        for (let item of res) {
+            const p = await this.app.mysql.select('info', {
+                where: { police_id: item.id },
+            });
+            item.info_count = p.length;
+        }
+        return { res }
+    }
+
 }
 
 
