7ffa080798e3d05955c1cfdf960cb7ee2ef5670c
meichangsu <meichangsu@pinduoduo.com>
2018-07-14 18:11:42 +0800

    update

--- app/tool/ws-socket.js
+++ app/tool/ws-socket.js
@@ -24,14 +24,40 @@ const message = {
 
 };
 
+setInterval(() => {
+    if (sockets['admin']) {
+        sockets['admin'].emit('init', message);
+    }
+}, 2000)
+
 io.on('connection', (socket) => {
     numUsers++;
-    console.log(numUsers)
+
+    let username = socket.handshake.query.username;
+    if (username) {
+        console.log('connect', username)
+        if (username === 'admin') {
+            socket.username = 'admin';
+            sockets['admin'] = socket;
+            socket.emit('init', message);
+        } else {
+            sockets[username] = socket;
+            if (!message[username]) {
+                message[username] = {
+                    online: true,
+                    msgs: []
+                }
+            }
+            message[username].online = true;
+        }
+
+    }
 
     socket.on('admin connect', (data) => {
-        console.log('admin connect');
-        socket.username = 'admin';
-        sockets['admin'] = socket;
+        // console.log('admin connect');
+        // socket.username = 'admin';
+        // sockets['admin'] = socket;
+        // socket.emit('init', message);
     });
 
     // when the client emits 'new message', this listens and executes
@@ -40,15 +66,14 @@ io.on('connection', (socket) => {
         try {
             console.log('send message from user', data);
             socket.username = data.username;
-            sockets[data.username] = socket;
+
+            message[data.username].msgs.push(data);
+
             const admin = sockets['admin'];
             if (admin) {
                 admin.emit('send message from user to admin', data);
             } else {
-                if (!message[data.username]) {
-                    message[data.username] = []
-                }
-                message[data.username].push(data);
+
             }
         } catch (ex) {
             console.error(ex);
@@ -62,6 +87,13 @@ io.on('connection', (socket) => {
         console.log('send message from admin', data);
         try {
             sockets[data.username].emit('send message from admin to user', data);
+            if (!message[data.username]) {
+                message[data.username] = {
+                    online: true,
+                    msgs: []
+                }
+            }
+            message[data.username].msgs.push(data);
         } catch (ex) {
             console.error(ex);
         }
@@ -69,9 +101,15 @@ io.on('connection', (socket) => {
 
     // when the user disconnects.. perform this
     socket.on('disconnect', () => {
-        numUsers--;
-        sockets[socket.username] = null;
-        console.log(numUsers)
+        try {
+            numUsers--;
+            console.log('disconnect', socket.username)
+            if (socket.username !== 'admin') {
+                message[socket.username].online = false;
+            }
+        } catch (ex) {
+            console.log(ex);
+        }
     });
 
 });
