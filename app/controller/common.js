d1fdabde2572023c9d49c660c97c9e04b4cf0658
meichangsu <meichangsu@pinduoduo.com>
2018-07-17 00:49:33 +0800

    update

--- app/controller/common.js
+++ app/controller/common.js
@@ -31,11 +31,12 @@ class CommonController extends Controller {
     }
 
     async getPoints() {
+        //
         // 12150828  9035  2018-07-15%2000:00:00  2018-07-15%2023:59:59
         const { vhcid, vehicle, begin_time, end_time } = this.ctx.request.body;
         const p = new Promise((resolve, reject) => {
-            superagent.get(`http://121.40.98.157:89/mygpsonline/json/vehicleAction_GetJsonVdata.action?vhcid=${vhcid}&vehicle=${vehicle}&begin_time=${begin_time}&end_time=${end_time}`)
-                .set('Cookie', 'JSESSIONID=84D9A6794DF7DFDBAD27A29090CB9022')
+            superagent.get(`http://121.40.98.157:89/mygpsonline/json/vehicleAction_GetJsonVdata.action?vhcid=${vhcid}&vehicle=${vehicle}&begin_time=${begin_time}&end_time=${end_time}&cret=&key=e617a19e0b2621bffb239dc0eb836bdf&datatype=0&intermin=0&timeorder=0&ip=port&dtype=&0`)
+                .set('Cookie', 'JSESSIONID=5CC79D497675A4A0D5C44955F08D27DC')
                 .end((res, rep) => {
                     resolve(rep)
                 });
