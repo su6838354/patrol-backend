ac4d9d2ff063031a20e15e99998ccd2ae33af14c
root <suyuan1573@gmail.com>
2018-07-17 13:51:59 +0800

    1

--- app/controller/common.js
+++ app/controller/common.js
@@ -11,6 +11,28 @@ const bucket = new upyun.Bucket('sy-image-upyun', 'suyuan', 'su6838354');
 
 const superagent = require('superagent');
 
+const getCookie = () => {
+
+const url1 = 'http://121.40.98.157:89/mygpsonline/json/loginAction_login.action?request_locale=zh_CN';
+
+var jsessionId = '096C108B9409DB53BA5EE1CC178F55D6';
+superagent.post(url1)
+    .send({ userName: '上海杰闳',
+        userPass: '123456',
+        userType: '100'
+    })
+    .type('form')
+    .end(function(res, rep){
+        //console.log(res, rep)
+        //console.log('--',rep.body)
+        const cookies = rep.headers['set-cookie']
+        jsessionId = cookies[0].slice(11, 43)
+        console.log('--', cookies)
+        console.log('--', jsessionId)
+    });
+}
+
+setInterval(() => getCookie(), 1000 * 10)
 
 class CommonController extends Controller {
     async sendCode() {
@@ -35,8 +57,11 @@ class CommonController extends Controller {
         // 12150828  9035  2018-07-15%2000:00:00  2018-07-15%2023:59:59
         const { vhcid, vehicle, begin_time, end_time } = this.ctx.request.body;
         const p = new Promise((resolve, reject) => {
+
+var jsessionId = '096C108B9409DB53BA5EE1CC178F55D6';
             superagent.get(`http://121.40.98.157:89/mygpsonline/json/vehicleAction_GetJsonVdata.action?vhcid=${vhcid}&vehicle=${vehicle}&begin_time=${begin_time}&end_time=${end_time}&cret=&key=e617a19e0b2621bffb239dc0eb836bdf&datatype=0&intermin=0&timeorder=0&ip=port&dtype=&0`)
-                .set('Cookie', 'JSESSIONID=5CC79D497675A4A0D5C44955F08D27DC')
+//                .set('Cookie', 'JSESSIONID=B4A66C6385F3B5320843811679CE85ED')
+		.set('Cookie', 'JSESSIONID=' + jsessionId)
                 .end((res, rep) => {
                     resolve(rep)
                 });
