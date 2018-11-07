c5e85e452b4ad95197a71bbf38c7c12d52d2849c
meichangsu <meichangsu@pinduoduo.com>
2018-11-07 15:11:26 +0800

    update

--- app/service/info.js
+++ app/service/info.js
@@ -7,15 +7,24 @@ const BaseService = ServiceFactory('info');
 
 class InfoService extends BaseService {
     async list(params) {
-        const { limit=10, offset = 0 } = params;
+        const { limit=10, offset = 0, start_time = 0, end_time = 2540805844 } = params;
         delete params.limit;
         delete params.offset;
-        const res = await this.app.mysql.select('info', {
-            where: { ...params },
-            orders: [['id','desc']],
-            limit,
-            offset,
-        });
+        // const res = await this.app.mysql.select('info', {
+        //     where: { ...params },
+        //     orders: [['id','desc']],
+        //     limit,
+        //     offset,
+        // });
+
+        let where = `where create_time >= ${start_time} and create_time <= ${end_time}`;
+        if(params.type) {
+            where = ` ${where} and type="${params.type}"`;
+        }
+        let sql = `select * from info ${where}`;
+        console.log(sql)
+        const res = await this.app.mysql.query(`${sql} limit ${offset}, ${limit}`);
+
         for (const item of res) {
             const polices = await this.app.mysql.select('staff', {
                 where: {id: item.police_id}
@@ -25,9 +34,10 @@ class InfoService extends BaseService {
                 item.police = police;
             }
         }
-        const allRes = await this.app.mysql.select('info', {
-            where: { ...params }
-        });
+        // const allRes = await this.app.mysql.select('info', {
+        //     where: { ...params }
+        // });
+        const allRes = await this.app.mysql.query(sql);
 
         const total = allRes.length;
         return { res, total }
